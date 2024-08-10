5aa974619d26955633d39a74d20f669198b7bfb5
Chimtamreddy <kr7348202@gmail.com>
2024-08-10 17:39:14 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 module "components" {
+  source           = "git::https://github.com/krish596/tf.module-basic-test.git"
   for_each = var.components
-  source = "git::https://github.com/krish596/roboshop-terraform.git"
 
-
-  zone_id = var.zone_id
-  security_group = var.security_group
-  name = each.value["name"]
-  instance_type = each.value["instance_type"]
-
+  zone_id           = var.zone_id
+  security_group    = var.security_group
+  name              = each.value["name"]
+  instance_type     = each.value["instance_type"]
 
 }
