c49dd6e4b91aa21c8d5c948e5e3adcc09c490bc3
Chimtamreddy <kr7348202@gmail.com>
2024-08-17 15:34:54 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "alb" {
   internal = each.value["internal"]
   lb_type = each.value["lb_type"]
   sg_ingress_cidr = each.value["sg_ingress_cidr"]
-  vpc_id = each.value["internal"] ? var.default_vpc_id : local.vpc_id
+  vpc_id = each.value["internal"] ? local.vpc_id : local.vpc_id
   subnets = each.value["internal"] ? local.app_subnets : data.aws_subnets.subnets.ids
   tags = var.tags
   env = var.env
