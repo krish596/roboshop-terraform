f3e69bc1d8d73fac1b523a3986fe7ddc0b21e4e0
Chimtamreddy <kr7348202@gmail.com>
2024-08-17 23:10:10 +0530

    Terraform

--- locals.tf
+++ locals.tf
@@ -1,3 +1,4 @@
 locals {
   vpc_id = lookup(lookup(var.vpc, "main", null), "vpc_id", null)
+  app_subnets = lookup(lookup(var.vpc, "main", null), "subnets", null)
 }
--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ module "vpc" {
 #   internal = each.value["internal"]
 #   lb_type = each.value["lb_type"]
 #   sg_ingress_cidr = each.value["sg_ingress_cidr"]
-#   vpc_id = var.default_vpc_id
+#   vpc_id = each.value["internal"] ? local.vpc_id : var.default_vpc_id
 #   tags = var.tags
 #   env = var.env
 #   sg_port= each.value["sg_port"]
